01b2d14ec85cd3c55cfa5acec433bbe9ecb2e140
carocastillar <72808394+carocastillar@users.noreply.github.com>
2022-06-19 13:37:52 -0500

    cambie todo

--- script.js
+++
@@ -1,7 +0,0 @@
-let parallax = document.querySelector('.parallax');
-
-window.onscroll = () => {
-    let scrollTop = document.documentElement.scrollTop;
-
-    parallax.style.backgroundPositionY = scrollTop * 0.7 + 'px';
-}
